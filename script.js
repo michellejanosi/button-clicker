9f8927a94aa288fde89fa472e900512bb5c322f9
michellejanosi <michelle.janosi@gmail.com>
2023-06-16 09:46:34 -0400

    Add button actions

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeText(element) {
+  return element.innerText = "Logout";
+}
+
+function removeElement(element) {
+  return element.remove();
+}
+
+function wasLiked(element) {
+  return alert("Ninja was liked")
+}
